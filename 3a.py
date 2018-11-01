b64a9c35ee0821c8bec74a625b9577da27b2f34d
Tandrac <tommy1196@gmail.com>
2018-10-31 23:04:27 -0400

    makeup

---
+++ 3a.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 22:02:01 2018
+
+@author: Tommy
+"""
+
+def bdpe(p,q,a,b,x0,m):
+    n = p*q
+
+def main():
+    p = 499
+    q = 547
+    a = -57
+    b = 52
+    x0 = 159201
+    m = [1,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0]
+    bgpe(p,q,a,b,x0,m)
+
+if __name__ == "__main__":
+    main()
